ede505f31ceb7a5ae1cc52e9defd8b120e1a1d37
another <893917884@qq.com>
2020-12-11 11:11:56 +0800

    目录改名

--- docs/.vuepress/config/theme.js
+++ docs/.vuepress/config/theme.js
@@ -3,12 +3,12 @@ const utilsRoot = path.join(__dirname, '..', '..','..', 'utils/modules');
 const docsRoot = path.join(__dirname,'..', '..');
 const readFile = require(utilsRoot + '/readFile');
 
-const 总标题 = readFile(docsRoot + '/总标题');
-const 配置 = [{
-    title: '配置标题',
+const 多目录 = readFile(docsRoot + '/多目录');
+const 单目录 = [{
+    title: '单目录标题',
     collapsable: false,
     sidebarDepth: 0,
-    children: readFile(docsRoot + '/配置')
+    children: readFile(docsRoot + '/单目录')
 }]
 
 
@@ -18,18 +18,18 @@ const themeConfig = {
   lastUpdated: '最后更新时间',
   nav: [
       {
-        text: '总标题',
-        link: '/总标题/01.标题1/01.标题1-1',
+        text: '多目录',
+        link: '/多目录/01.章节1/01.章节1-1',
       },
       {
-        text: '配置',
-        link: '/配置/配置1',
+        text: '单目录',
+        link: '/单目录/01.文章一',
       },
   ],
   
   sidebar: {
-    '/总标题/': 总标题,
-    '/配置/': 配置,
+    '/多目录/': 多目录,
+    '/单目录/': 单目录,
   },
 };
 
