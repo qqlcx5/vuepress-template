d11393c7938574e076a1a500038d7cb5a22fa840
qqlcx5 <qqlcx5@163.com>
2021-02-19 10:48:25 +0800

    pwa

--- docs/.vuepress/config/plugins.js
+++ docs/.vuepress/config/plugins.js
@@ -1,8 +1,4 @@
 const plugins = [
-    ['@vuepress/pwa', {
-        serviceWorker: true, //用于缓存页面的内容以供离线使用
-        updatePopup: true
-    }],
     ['@vuepress/last-updated',{
         transformer: (timestamp) => {
         const dayjs = require('dayjs')
