66d17b996bd6598aae05438bfff3d863a1942524
qqlcx5 <qqlcx5@163.com>
2021-09-27 23:59:15 +0800

    🚀 init

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,7 +1,7 @@
 const plugins = require('./config/plugins');
 const themeConfig = require('./config/theme');
 module.exports = {
-  base: '/vuepress-template/',
+  theme: 'reco',
   title: 'vuepress-template',
   description: '前端博客,个人技术博客,前端,前端开发,前端框架,web前端,前端面试题,技术文档,学习,面试,JavaScript,js,ES6,TypeScript,vue,python,css3,html5,Node,git,github,markdown',
   head: [
